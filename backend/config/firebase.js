aaaf5b096c0951334c4a25b4e16b907636713f65
clintmadeit <clintonbrirdley@gmail.com>
2025-09-05 12:34:00 +0300

    Exclude backend folder from mobile branch tracking

--- backend/config/firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // ignoreUndefinedProperties: true
-});
-
-module.exports = admin;
