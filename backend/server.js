0e0be8a2dc4a3b20c4f309d2981049cb4dd7edfe
clintmadeit <clintonbrirdley@gmail.com>
2025-09-24 14:44:20 +0300

    Trigger redeploy with backend fixes
    - Force redeploy to ensure hasAnySubscription fix is deployed
    - Company creation should work after this deployment

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,4 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Deployment test Wed Sep 24 01:24:29 PM EAT 2025
-console.log('Backend deployment test - Wed Sep 24 02:02:59 PM EAT 2025');
+console.log('Backend deployment test - Wed Sep 24 02:45:00 PM EAT 2025 - Fixed hasAnySubscription');
